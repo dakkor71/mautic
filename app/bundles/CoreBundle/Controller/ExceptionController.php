75ce43ffc744eb297e4580830c70156d2b927dd4
lan <lan@webmecanik.com>
2016-03-03 16:16:51 +0100

    Modification  contact support en cas d'erreur

--- app/bundles/CoreBundle/Controller/ExceptionController.php
+++ app/bundles/CoreBundle/Controller/ExceptionController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
+use Mautic\CoreBundle\Helper\InputHelper;
 
 /**
  * Class ExceptionController
@@ -32,12 +33,8 @@ class ExceptionController extends CommonController
             $env            = $this->factory->getEnvironment();
             $currentContent = $this->getAndCleanOutputBuffering($request->headers->get('X-Php-Ob-Level', -1));
             $layout         = $env == 'prod' ? 'Error' : 'Exception';
-            $code           = $exception->getStatusCode();
-            if ($code === 0) {
-                //thrown exception that didn't set a code
-                $code = 500;
-            }
-
+            $code = $this->extractCode($exception);
+         
             // Special handling for oauth and api urls
             if ((strpos($request->getUri(), '/oauth') !== false && strpos($request->getUri(), 'authorize') === false) || strpos($request->getUri(), '/api') !== false) {
                 $dataArray = array(
@@ -75,7 +72,23 @@ class ExceptionController extends CommonController
 
             $url      = $request->getRequestUri();
             $urlParts = parse_url($url);
-
+            
+            /***/
+            $currentUser = $this->factory->getUser();
+            $completUrl = $_SERVER['HTTP_HOST'].$url;
+            
+           // envoi du mail automatique au support
+           if($env === 'prod') {
+           	$this->sendMailSupportAutomatique($currentUser, $completUrl, $exception);
+           }
+         
+          	// construction du mail
+           $subject =  $this->buildSubjectMail($code, $completUrl,  false);
+           $body = $this->buildBodyMailFromException($currentUser, $completUrl, $exception, false);
+           $mailSupport = "support@webmecanik.com?subject=".$subject."&body=".$body;
+           
+            /***/
+            
             return $this->delegateView(
                 array(
                     'viewParameters'  => array(
@@ -85,7 +98,8 @@ class ExceptionController extends CommonController
                         'exception'      => $exception,
                         'logger'         => $logger,
                         'currentContent' => $currentContent,
-                        'isPublicPage'   => $anonymous
+                        'isPublicPage'   => $anonymous, 
+                    	'mailSupport' => $mailSupport
                     ),
                     'contentTemplate' => $template,
                     'passthroughVars' => array(
@@ -116,5 +130,91 @@ class ExceptionController extends CommonController
         Response::closeOutputBuffers($startObLevel + 1, true);
 
         return ob_get_clean();
+    }
+    
+    /**
+     * 
+     */
+    protected function extractCode($exception)  {
+    	$code  = $exception->getStatusCode();
+    	if ($code === 0) {
+    		//thrown exception that didn't set a code
+    		$code = 500;
+    	}	
+    	return $code;
+    }
+    
+    /**
+     * construct subject mail
+     */
+    protected function buildSubjectMail($code, $url, $auto) {
+    	if($auto)
+    		$subject  = "Erreur code ".$code." -  ".$url;
+    	else 
+    		$subject  = "Demande de support - Code : ".$code;
+    	return $subject ;
+    }
+    
+    protected function buildBodyMailFromException ($user, $url, $exception, $auto) {
+    	$code = $this->extractCode($exception);
+    	$errorMessage = $exception->getMessage();
+    	$stack = $exception->getTrace();
+    	
+    	return $this->buildBodyMail($code, $errorMessage, $url, $stack, $user, $auto);
+    }
+    /**
+     * construct body mail
+     */
+    protected function buildBodyMail($code, $errorMessage, $url, $stack, $user, $auto) {
+    
+    	$pile = $this->renderView('MauticCoreBundle:Exception:traces.html.php', array('traces' => $stack ));
+    	if($auto) {
+    		
+    		$body = '<!DOCTYPE html> ';
+    		$body .= '<html>';
+    		$body .= '<body> ';
+    		$body .= '<strong>MAIL AUTOMATIQUE</strong>  <br/> <br/> ';
+    		$body .= 'User : '.$user->getName().', '.$user->getEmail().' <br/> ' ;
+    		$body .= 'Type d′erreur : '.$code .' '.$errorMessage.' <br/>';
+    		$body .= 'Url d′erreur : '.$url.' <br/> ';
+    		$body .= 'Pile : ' . $pile. ' ';
+    		$body .= '</body> ';
+    		$body .= '</html> ';
+    		
+    	} else {
+    		
+    		$body = 'Votre identité : '.$user->getName().', '. $user->getEmail()." %0D%0A %0D%0A";
+    		$body .= 'Ce que vous vouliez faire : '."%0D%0A %0D%0A";
+    		$body .= 'Les actions que vous avez faites : '."%0D%0A %0D%0A";
+    		$body .= 'Ce qui s\'est passé : '."%0D%0A %0D%0A";
+    		$body .= 'Informations complémentaires : '."%0D%0A %0D%0A";
+    		$body .= '*** NE PAS EFFACER CI DESSOUS - INFORMATIONS POUR LE SUPPORT ***'."%0D%0A";
+    		$body .= 'URL d\'erreur : '."$url %0D%0A";
+    		$body .= 'Type d\'erreur : '."$code $errorMessage %0D%0A ";
+    		
+    	}
+    	return $body;
+    }
+    
+    protected function sendMailSupportAutomatique ($user, $url, $exception) {
+    	$code = $this->extractCode($exception);
+    	$errorMessage = $exception->getMessage();
+    	$stack = $exception->getTrace();
+    	
+    	$subjectAuto =  $this->buildSubjectMail($code, $url,  true);
+    	$bodyAuto = $this->buildBodyMailFromException($user, $url, $exception, true) ;
+    	
+    	$subject = InputHelper::clean($subjectAuto);
+    	// $body    = InputHelper::clean($bodyAuto);
+    	
+    	$message = \Swift_Message::newInstance()
+    		->setSubject($subjectAuto)
+    		->setFrom(array($user->getEmail() => $user->getName()))
+    		->setTo("support@webmecanik.com")
+    		->setCharset('utf-8')
+    		->setContentType("text/html")
+    		->setBody($bodyAuto, 'text / html');
+    	
+    	$this->get('mailer')->send($message);
     }
 }
