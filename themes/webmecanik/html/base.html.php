3d4a9302dd4b2068f713e3b965194e987d83b561
David C <dco@webmecanik.com>
2016-03-17 10:19:32 +0100

    Ajustement view port sur le theme webmecanik

--- themes/webmecanik/html/base.html.php
+++ themes/webmecanik/html/base.html.php
@@ -15,6 +15,7 @@
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" type="text/css" />
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="<?php echo $view['assets']->getUrl('themes/webmecanik/css/style.css'); ?>" type="text/css" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0" />
         <?php $view['assets']->outputHeadDeclarations(); ?>
     </head>
     <body>
